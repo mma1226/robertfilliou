352518a942046a3d07b4e103958c6178d72991c0
mma1226 <manxue.ma@gmail.com>
2023-04-09 13:55:37 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,73 @@
+let font1;
+
+let cylinderRadius = 220;
+let cylinderWidth = 700;
+let textTexture;
+let indexWord = 0;
+let words = ['ROBERT FILLIOU MULTIPLES & EDITIONS'];
+
+
+function preload() {
+  font1 = loadFont('BastardoGroteskish_Bold.otf');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight,WEBGL);
+	
+  textTexture = createGraphics(2*PI*cylinderRadius,windowHeight*2);
+	textTexture.colorMode(HSB);	
+	textTexture.background(255);
+  textTexture.fill(255);
+	textTexture.textFont(font1);
+  textTexture.textAlign(CENTER);
+  textTexture.textSize(80);
+
+}
+
+function draw() {
+	//scale(1, -1);  // uncomment this line to save an image or thumb. Then comment out the line.
+	background(0);
+	noStroke();
+	
+
+	var rotation = 1;
+
+	let wave = (sin(frameCount * 0.005 + (0.005) * 0.005) * 1);
+	
+	
+	textTexture.background(0);
+	for(let i = 0; i <=55; i++){
+		textTexture.fill(226,100,100);
+		textTexture.text(words[indexWord], PI*cylinderRadius,i*80);
+	}
+	
+	//image(textTexture, -1000,-1000);
+	
+	translate(-windowWidth, -100);
+	for(let i = 0; i <= 10; i++){
+		translate(cylinderRadius*2+100, 0);
+		push();
+		
+		rotateZ(radians(45));
+		push();
+		
+    if(i%2==0){
+      rotateY(-frameCount * 0.01);  
+    }else{
+      rotateY(frameCount * 0.02);  
+    }
+    
+		texture(textTexture);
+		cylinder(cylinderRadius, windowHeight*2,24);
+		pop();
+		pop();
+	}	
+}
+
+function changeWord() {
+	indexWord++;
+	if(indexWord > 4){
+		indexWord = 0;
+	}
+	setTimeout(changeWord, 500)
+}
